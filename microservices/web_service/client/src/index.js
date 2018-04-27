6197794580e54b4866cd1c42eeec7f2d15415842
mattander91 <mattander91@gmail.com>
2018-04-27 15:31:00 -0700

    removed comments

--- microservices/web_service/client/src/index.js
+++ microservices/web_service/client/src/index.js
@@ -1,15 +1,3 @@
-/*
-
-2.) micro_messenger_local - for deployment
-      - remove test files
-     - remove docker files
-     - Check ajax URLs in each service
-     - Set up database for each service
-     - Webpack for web_service?
-
-*/
-
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.js';
